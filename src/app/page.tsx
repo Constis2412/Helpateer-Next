84db0fd726e2b99a79725aa35b7e1ee62bcd9dcc
Constis2412 <pfe20424@spengergasse.at>
2024-04-29 08:53:56 +0200

    fortschritt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,42 @@
+import Header from "@/components/Header";
+
 export default function Home() {
-  return <main className="text-center"> Hello World</main>;
+  return (
+    <main className="h-svh overflow-hidden">
+      {" "}
+      <Header />
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit, veniam
+      reprehenderit? Provident iste quibusdam doloremque? Repellendus
+      reprehenderit voluptatem dolorem expedita natus perspiciatis explicabo
+      labore ratione iusto laboriosam, exercitationem eveniet ut animi. Odit
+      tempora suscipit enim sunt, architecto sint ut maxime iure adipisci
+      necessitatibus laboriosam saepe dicta earum facilis quos dignissimos? Quia
+      modi eum error placeat ullam temporibus officia eveniet ipsum nam nihil,
+      maxime id iste ipsam veritatis aliquam repudiandae similique consectetur
+      soluta eaque voluptates? Accusamus tempora consequatur a sint excepturi!
+      Esse voluptate vero explicabo magnam, asperiores, sapiente impedit ab eos,
+      totam nobis cupiditate quos error ratione in cum beatae quas distinctio
+      exercitationem tenetur. Deserunt officia possimus porro vero ipsa dolor
+      nihil quam earum harum aut, beatae non provident eos quis deleniti rem
+      mollitia nisi magnam minima unde iste, dignissimos sapiente! Qui ipsa
+      itaque eaque suscipit repellendus velit ut? Sunt eligendi numquam,
+      consectetur quisquam quasi enim deserunt ducimus ipsam at id incidunt eius
+      optio est quod alias quidem officiis illum nobis vitae asperiores
+      cupiditate, ipsa velit ipsum. Nihil nobis, facere repudiandae veritatis
+      quia vel temporibus cumque natus itaque saepe ratione. Consectetur,
+      aperiam rerum. Ullam perferendis perspiciatis porro deleniti, nulla dolor
+      autem corporis pariatur adipisci animi unde nesciunt exercitationem atque
+      ea commodi. Officiis, vel. Saepe aliquam, vel non facilis assumenda
+      deserunt fugit dignissimos nostrum in officiis pariatur quod, sapiente
+      magni. Neque nulla inventore facilis velit mollitia unde sapiente
+      blanditiis laborum saepe. Unde labore reiciendis id ducimus itaque impedit
+      consectetur ipsa. Error, dicta. Ea alias assumenda nihil architecto
+      similique esse, est nam autem voluptatibus mollitia, perferendis rem
+      dolore harum reprehenderit vel ipsam suscipit. Aut illum deserunt maiores
+      possimus cumque cupiditate sit. Obcaecati similique dignissimos, cumque
+      nostrum, iusto quod quasi pariatur facere tempore, maxime illum ut
+      doloribus minus! Voluptates minima eveniet sunt tenetur officiis, hic
+      officia ipsum eius, voluptatem iure debitis dicta accusantium? Optio.
+    </main>
+  );
 }
