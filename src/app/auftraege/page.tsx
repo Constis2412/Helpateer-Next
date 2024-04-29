214223a0466d10a4114827bcaed18baf2807a465
Bre20228 <Bre20228@spengergasse.at>
2024-04-29 12:47:18 +0200

    Auftragseite colums, button

---
+++ src/app/auftraege/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const AuftraegeSeite = () => {
+  return (
+    <div className="grid grid-cols-3 gap-4 h-screen">
+      <div className="col-span-1 bg-cyan-400 p-4" >
+        
+        
+      </div>
+      <div className="col-span-2"> 
+      <button className="btn btn-info">Auftrag erstellen</button> 
+      </div>
+    </div>
+  );
+};
+
+
+
+export default AuftraegeSeite;
