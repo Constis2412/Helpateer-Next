5b083227bb38f8d8833a4dbd2745b068a52b548d
Constis2412 <pfe20424@spengergasse.at>
2024-05-07 13:08:47 +0200

    header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,11 +3,13 @@ import Modal from "./Modal";
 
 const Header = () => {
   return (
-    <div
-      className="hero h-[40%] bg-base-200"
-      style={{ backgroundImage: "url(bled-2582655_1920.jpg)" }}
-    >
-      <div className="hero-content text-center">
+    <div className="hero h-[40%] relative">
+      <img
+        src="bled-2582655_1920.jpg"
+        alt="background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="hero-content text-center z-10 relative">
         <div className="max-w-md">
           <h1 className="text-5xl font-bold">Welcome to Helpateer</h1>
           <p className="py-6">
